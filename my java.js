b5bf83048b24e44a6ecdd9fb155288fff4401749
ravikoniki <87050134+ravikoniki@users.noreply.github.com>
2021-09-11 21:46:49 +0530

    Add files via upload

---
+++ my java.js
@@ -0,0 +1,7 @@
+function para() {
+    alert('hello');
+    document.getElementById('para').innerHTML="this is original text";
+    console.log('hello world');
+    console.log('i am ravi');
+    document.write('hello iam ravi');            
+}
